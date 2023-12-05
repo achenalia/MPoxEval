aa1e756442e030a36566dd1dd3a759d87cdf711f
Esmé Taylor Richardson <reese.taylor.richardson@emory.edu>
2023-12-05 17:29:58 -0500

    updated questions to have better explanation for users

--- MPoxEval.py
+++ MPoxEval.py
@@ -6,7 +6,7 @@ from keras.layers import Dense
 
 # Read the dataset provided and outline the different categories necessary:
 data = pd.read_csv("MPox_Dataset.csv")
-features = ['Systemic Illness', 'Rectal Pain', 'Sore Throat', 'Oedema below the Waist', 'Oral Lesions',
+features = ['Rectal Pain', 'Sore Throat', 'Oedema below the Waist', 'Oral Lesions',
             'Solitary Lesion', 'Swollen Tonsils', 'HIV Infection', 'Sexually Transmitted Infection']
 target = 'MonkeyPox'
 
@@ -15,9 +15,11 @@ encoder = LabelEncoder()
 for feature in features:
     data[feature] = encoder.fit_transform(data[feature])
 
+# Encode target variable 'MonkeyPox' with LabelEncoder
+y = encoder.fit_transform(data[target])
+
 # Create training and testing sets:
 X = data[features]
-y = data[target]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Establish the neural network we will use:
@@ -36,69 +38,85 @@ print(f"Accuracy of the ANN: {accuracy * 100:.4f}")
 print("Welcome to the MPox Test Recommender System -- an Artificial Neural Network (ANN) trained on anonymized "
       "patient data.")
 print(f"This ANN has an accuracy of {accuracy * 100:.4f}.")
-print("What type of Systemic Illness are you experiencing?")
-systemic_illness = input().strip().lower()
 
-while systemic_illness not in ['fever', 'muscle aches and pain', 'swollen lymph nodes']:
-    print("Please enter a valid Systemic Illness: Fever, Muscle Aches and Pain, or Swollen Lymph Nodes.")
+# Ask 'Systemic Illness' question separately
+print("Are you experiencing symptoms typical of a Systemic Illness such as Fever, Fatigue, Swollen Lymph Nodes, "
+      "or Muscle Weakness? (Yes/No): ")
+systemic_illness = input().strip().lower()
+while systemic_illness not in ['yes', 'no']:
+    print("Please enter a valid response: Yes or No.")
     systemic_illness = input().strip().lower()
 
-# Init the question array to be asked:
-questions = [
-    "Are you experiencing Rectal Pain? Provide your Answer as Yes or No.",
-    "Are you experiencing Sore Throat? Provide your Answer as Yes or No.",
-    "Are you experiencing any Oedema (swelling) below the waist? Provide your Answer as Yes or No.",
-    "Do you have any Oral Lesions (ie. sores)? Provide your Answer as Yes or No.",
-    "Do you have any Solitary Lesions (ie. sores) in any part of your body? Provide your Answer as Yes or No.",
-    "Do you have swollen tonsils? Provide your Answer as Yes or No.",
-    "Have you been diagnosed with an HIV Infection by your physician? Provide your Answer as Yes or No.",
-    "Have you been diagnosed with any Sexually Transmitted Infections (STIs) by your physician? Provide your Answer "
-    "as Yes or No."
-]
+# Initialize user input dictionary
+user_input = {}
 
-# Set up the algorithm which will allow for answers from the user:
-answers = []
-for question in questions:
-    answer = input(question).strip().lower()
-    while answer not in ['yes', 'no']:
-        print("Please enter a valid response: Yes or No.")
-        answer = input(question).strip().lower()
-    answers.append(answer)
+# Ask each feature's question separately and gather user responses
+print("\nAnswer the following questions:")
 
-# Define mappings for categorical values
-categorical_mappings = {
-    'fever': 0,
-    'muscle aches and pain': 1,
-    'swollen lymph nodes': 2,
-    'yes': 1,
-    'no': 0
-}
+# Ask 'Rectal Pain' question
+user_input['Rectal Pain'] = input("Are you suffering from Rectal Pain? (Yes/No): ").strip().lower()
+while user_input['Rectal Pain'] not in ['yes', 'no']:
+    print("Please enter a valid response: Yes or No.")
+    user_input['Rectal Pain'] = input("Are you suffering from Rectal Pain? (Yes/No): ").strip().lower()
 
-# Consolidate the answers to use for prediction later:
-user_input = {
-    'Systemic Illness': systemic_illness,
-    'Rectal Pain': answers[0],
-    'Sore Throat': answers[1],
-    'Oedema below the Waist': answers[2],
-    'Oral Lesions': answers[3],
-    'Solitary Lesion': answers[4],
-    'Swollen Tonsils': answers[5],
-    'HIV Infection': answers[6],
-    'Sexually Transmitted Infection': answers[7]
-}
+# Ask 'Sore Throat' question
+user_input['Sore Throat'] = input("Are you suffering from a Sore Throat? (Yes/No): ").strip().lower()
+while user_input['Sore Throat'] not in ['yes', 'no']:
+    print("Please enter a valid response: Yes or No.")
+    user_input['Sore Throat'] = input("Are you suffering from a Sore Throat? (Yes/No): ").strip().lower()
 
-# Map categorical inputs to encoded values
-for feature in features:
-    user_input[feature] = categorical_mappings.get(user_input[feature], user_input[feature])
+# Ask 'Oedema below the Waist' question
+user_input['Oedema below the Waist'] = input("Are you experiencing Oedema (swelling) below the Waist? (Yes/No): ").strip().lower()
+while user_input['Oedema below the Waist'] not in ['yes', 'no']:
+    print("Please enter a valid response: Yes or No.")
+    user_input['Oedema below the Waist'] = input("Are you experiencing Oedema (swelling) below the Waist? (Yes/No): ").strip().lower()
+
+# Ask 'Oral Lesions' question
+user_input['Oral Lesions'] = input("Are you suffering from Oral Lesions (ie. sores)? (Yes/No): ").strip().lower()
+while user_input['Oral Lesions'] not in ['yes', 'no']:
+    print("Please enter a valid response: Yes or No.")
+    user_input['Oral Lesions'] = input("Are you suffering from Oral Lesions (ie. sores)? (Yes/No): ").strip().lower()
+
+# Ask 'Solitary Lesion' question
+user_input['Solitary Lesion'] = input("Are you suffering from Solitary Lesions (ie. sores) anywhere on your body? ("
+                                      "Yes/No): ").strip().lower()
+while user_input['Solitary Lesion'] not in ['yes', 'no']:
+    print("Please enter a valid response: Yes or No.")
+    user_input['Solitary Lesion'] = input("Are you suffering from Solitary Lesions (ie. sores) anywhere on your body? "
+                                          "(Yes/No): ").strip().lower()
+
+# Ask 'Swollen Tonsils' question
+user_input['Swollen Tonsils'] = input("Are you suffering from Swollen Tonsils? (Yes/No): ").strip().lower()
+while user_input['Swollen Tonsils'] not in ['yes', 'no']:
+    print("Please enter a valid response: Yes or No.")
+    user_input['Swollen Tonsils'] = input("Are you suffering from Swollen Tonsils? (Yes/No): ").strip().lower()
+
+# Ask 'HIV Infection' question
+user_input['HIV Infection'] = input("Has your physician diagnosed you with an HIV Infection? (Yes/No): ").strip().lower()
+while user_input['HIV Infection'] not in ['yes', 'no']:
+    print("Please enter a valid response: Yes or No.")
+    user_input['HIV Infection'] = input("Has your physician diagnosed you with an HIV Infection? (Yes/No): ").strip().lower()
+
+# Ask 'Sexually Transmitted Infection' question
+user_input['Sexually Transmitted Infection'] = input("Has your physician diagnosed you with a Sexually Transmitted "
+                                                     "Infection (STI)? (Yes/No): ").strip().lower()
+while user_input['Sexually Transmitted Infection'] not in ['yes', 'no']:
+    print("Please enter a valid response: Yes or No.")
+    user_input['Sexually Transmitted Infection'] = input("Has your physician diagnosed you with a Sexually Transmitted "
+                                                         "Infection (STI)? (Yes/No): ").strip().lower()
+
+# Transform user input into DataFrame for prediction:
+user_data = pd.DataFrame([user_input])
 
 # Use the ANN to create predictions to output:
-user_data = pd.DataFrame([user_input])
 prediction = model.predict(user_data[features])
 
 # Print predictions to user:
-if prediction >= 0.6:
-    print("Based on the data you provided, you are recommended to get tested for Monkey-Pox as soon as possible.")
+if prediction >= 0.6 or systemic_illness == 'yes':
+    print("\nBased on the data you provided, you are recommended to get tested for Monkey-Pox as soon as possible.")
+elif all(value == 'no' for value in user_input.values()):
+    print("\nYou answered 'No' to all symptoms. It is recommended to consult a healthcare professional for evaluation.")
 else:
-    print("Based on the data you provided, it does not appear that you need to be tested for Monkey-Pox, however, "
-          "due to your symptoms, you are recommended to seek care from a healthcare professional at your earliest "
+    print("\nBased on the data you provided, it does not appear that you need to be tested for Monkey-Pox. However, "
+          "due to your symptoms, it is recommended to seek care from a healthcare professional at your earliest "
           "convenience.")
